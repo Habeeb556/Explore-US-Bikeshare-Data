03c3db1220ac438e46dd960dc16a2ac47f9d54cf
Habeeb556 <ahmed556@std.mans.edu.eg>
2021-06-25 08:17:59 +0200

    Fix case-sensitive input problem

--- bikeshare.py
+++ bikeshare.py
@@ -21,7 +21,7 @@ def get_filters():
 
 
     while True:
-      city = input("\nWhich city would you like to filter by? New York City, Chicago or Washington?\n")
+      city = input("\nWhich city would you like to filter by? New York City, Chicago or Washington?\n").lower()
       if city not in ('New York City', 'Chicago', 'Washington'):
         print("Sorry, your input should be: chicago new york city or washington.")
         continue
@@ -31,7 +31,7 @@ def get_filters():
     # TO DO: get user input for month (all, january, february, ... , june)
 
     while True:
-      month = input("\nWhich month would you like to filter by? January, February, March, April, May, June or type 'all' if you do not have any preference?\n")
+      month = input("\nWhich month would you like to filter by? January, February, March, April, May, June or type 'all' if you do not have any preference?\n").lower()
       if month not in ('January', 'February', 'March', 'April', 'May', 'June', 'all'):
         print("Sorry, your input should be: January, February, March, April, May, June or type 'all'.")
         continue
@@ -41,7 +41,7 @@ def get_filters():
     # TO DO: get user input for day of week (all, monday, tuesday, ... sunday)
 
     while True:
-      day = input("\nAre you looking for a particular day? If so, kindly enter the day as follows: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday or type 'all' if you do not have any preference.\n")
+      day = input("\nAre you looking for a particular day? If so, kindly enter the day as follows: Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday or type 'all' if you do not have any preference.\n").lower()
       if day not in ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'all'):
         print("Sorry, your input should be: Sunday, Monday, ... Friday or type 'all'.")
         continue
